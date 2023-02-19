49ca0234c1d9acf7f87c2084480bec94f465317d
hustle-dev <dlwoabsdk@gmail.com>
2023-02-19 16:28:46 +0900

    feat: Workshop css done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import PerspectiveBar from "./components/PerspectiveBar/PerspectiveBar";
 
 function App() {
   return (
     <>
+      <PerspectiveBar />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./PerspectiveBar.module.css";
+
+const PerspectiveBar = () => {
+  return <div className={styles.perspectiveBar}></div>;
+};
+
+export default PerspectiveBar;
